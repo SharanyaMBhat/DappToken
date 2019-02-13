1a0cec2682241ed1593edfe0e3f5886501740c39
SharanyaMBhat <sharanyamundakana@gmail.com>
2019-02-13 17:48:17 +0530

    4. creating crowd sale smart contract

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -60,8 +60,7 @@ contract DappToken
         require(_value <= allowance[_from][msg.sender],"error");
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-
-      allowance[_from][msg.sender] -= _value;
+        allowance[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
         
         return true;
